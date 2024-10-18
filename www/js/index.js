416aacf00975308c8e5612ed0cf4459c8907c7f2
hikmah dikko <dhywis31@gmail.com>
2024-10-18 13:10:43 +0100

    new snippet

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,23 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+FCMPlugin.getToken(function(token) {
+    console.log("FCM Token: " + token);
+    // You can store this token or send it to your server for further use
+}, function(err) {
+    console.error("Error retrieving FCM token: " + err);
+});
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+// Handle incoming notifications
+FCMPlugin.onNotification(function(data) {
+    console.log("Notification data: " + JSON.stringify(data));
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+    if (data.wasTapped) {
+        // Notification received in background
+        alert("Background Notification: " + JSON.stringify(data));
+    } else {
+        // Notification received in foreground
+        alert("Foreground Notification: " + JSON.stringify(data));
+    }
+}, function(msg) {
+    console.log("Notification callback success: " + msg);
+}, function(err) {
+    console.error("Notification callback error: " + err);
+});
